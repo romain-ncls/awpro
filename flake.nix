5e74eafbe67fe86e4eceb78a039f697083ac1fae
Romain Nicolas <nicolasromain0@gmail.com>
2026-04-30 15:43:34 +0200

    build: add udev root to flake setup

--- flake.nix
+++ flake.nix
@@ -26,6 +26,26 @@
         awpro = (pkgsFor system).pkgsStatic.callPackage ./default.nix { };
         default = self.packages.${system}.awpro;
       });
+      nixosModules.default = { pkgs, lib, config, ... }:
+        let
+          udev-rules = pkgs.writeTextFile {
+            name = "awpro-udev-rules";
+            destination = "/lib/udev/rules.d/70-awpro.rules";
+            text = ''
+              ACTION!="add|change", GOTO="awpro_end"
+              # Dell AW Pro headset (413c:a529)
+              SUBSYSTEM=="hidraw", ATTRS{idVendor}=="413c", ATTRS{idProduct}=="a529", TAG+="uaccess"
+              LABEL="awpro_end"
+            '';
+          };
+        in
+        {
+          options.programs.awpro.enable = lib.mkEnableOption "awpro headset tool";
+          config = lib.mkIf config.programs.awpro.enable {
+            services.udev.packages = [ udev-rules ];
+            environment.systemPackages = [ self.packages.${pkgs.system}.awpro ];
+          };
+        };
       devShells = forAllSystems (system: {
         default = (pkgsFor system).callPackage ./shell.nix { };
       });
